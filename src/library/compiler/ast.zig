07a8c0ee6d2f708a2cdb86f8f05b7af160db6c21
UnusualEgg <shotgun282006@gmail.com>
2026-01-12 14:34:58 -0600

    WIP collapse constatns

--- src/library/compiler/ast.zig
+++ src/library/compiler/ast.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const Location = @import("location.zig").Location;
+const Diagnostics = @import("diagnostics.zig").Diagnostics;
 
 pub const UnaryOperator = enum {
     negate,
@@ -73,6 +74,224 @@ pub const Expression = struct {
             rhs: *Expression,
         },
     };
+
+    pub fn optimzie(self: Expression, consts: std.StringHashMap(Expression), array_allocator: std.mem.Allocator, diag: *Diagnostics) !?Expression {
+        switch (self.type) {
+            //comptime calls not currently supported
+            .function_call, .method_call => return null,
+            .variable_expr => |var_name| {
+                if (std.mem.eql(u8, var_name, "true") or std.mem.eql(u8, var_name, "false") or std.mem.eql(u8, var_name, "void")) {
+                    return self;
+                }
+                if (consts.get(var_name)) |value| {
+                    return value.optimzie(consts, array_allocator);
+                } else return null;
+            },
+            .unary_operator => |op| {
+                switch (op.operator) {
+                    .boolean_not => {
+                        if (try op.value.optimzie(consts, array_allocator)) |value| {
+                            switch (value.type) {
+                                .variable_expr => |var_name| {
+                                    if (std.mem.eql(u8, var_name, "true")) {
+                                        return Expression{ .location = self.location, .type = .{ .variable_expr = "false" } };
+                                    } else if (std.mem.eql(u8, var_name, "false")) {
+                                        return Expression{ .location = self.location, .type = .{ .variable_expr = "true" } };
+                                    } else {
+                                        unreachable;
+                                    }
+                                },
+                                else => return error.InvalidCode,
+                            }
+                        } else return null;
+                    },
+                    .negate => {
+                        if (try op.value.optimzie(consts, array_allocator)) |value| {
+                            switch (value.type) {
+                                .number_literal => |num| {
+                                    return Expression{ .location = self.location, .type = .{ .number_literal = -num } };
+                                },
+                                else => return error.InvalidCode,
+                            }
+                        } else return null;
+                    },
+                }
+            },
+            .binary_operator => |op| {
+                const lhs_expr = try op.lhs.optimzie(consts, array_allocator, diag);
+                if (lhs_expr) |lhs| {
+                    const rhs_expr = try op.lhs.optimzie(consts, array_allocator, diag);
+                    if (rhs_expr) |rhs| {
+                        switch (op.operator) {
+                            .add => {
+                                switch (lhs.type) {
+                                    .string_literal => |str| {
+                                        switch (rhs.type) {
+                                            .string_literal => |rhs_str| {
+                                                const concat = try std.mem.concat(array_allocator, u8, &.{ str, rhs_str });
+                                                return Expression{ .location = self.location, .type = .{ .string_literal = concat } };
+                                            },
+                                            else => {
+                                                diag.emit(
+                                                    .@"error",
+                                                    op.lhs.location,
+                                                    "Expected type at {f} to be a string based on lhs of add operation",
+                                                    .{rhs.location},
+                                                );
+                                                return error.InvalidCode;
+                                            },
+                                        }
+                                    },
+                                    .array_literal => |arr| {
+                                        switch (rhs.type) {
+                                            .array_literal => |rhs_array| {
+                                                const concat = try std.mem.concat(array_allocator, u8, &.{ arr, rhs_array });
+                                                return Expression{ .location = self.location, .type = .{ .array_literal = concat } };
+                                            },
+                                            else => {
+                                                diag.emit(
+                                                    .@"error",
+                                                    op.lhs.location,
+                                                    "Expected type at {f} to be an array based on lhs of add operation",
+                                                    .{rhs.location},
+                                                );
+                                                return error.InvalidCode;
+                                            },
+                                        }
+                                    },
+                                    .number_literal => |num| {
+                                        switch (rhs.type) {
+                                            .number_literal => |rhs_num| {
+                                                return Expression{ .location = self.location, .type = .{ .number_literal = num + rhs_num } };
+                                            },
+                                            else => {
+                                                diag.emit(
+                                                    .@"error",
+                                                    op.lhs.location,
+                                                    "Expected rhs type to be a number based on lhs of add operation",
+                                                    .{},
+                                                );
+                                                return error.InvalidCode;
+                                            },
+                                        }
+                                    },
+                                    else => {
+                                        diag.emit(
+                                            .@"error",
+                                            lhs.location,
+                                            "Expected type to be a string, number, or array for an add operation",
+                                            .{},
+                                        );
+                                        return error.InvalidCode;
+                                    },
+                                }
+                            },
+                            .subtract => {
+                                switch (lhs.type) {
+                                    .number_literal => |num| {
+                                        switch (rhs.type) {
+                                            .number_literal => |rhs_num| {
+                                                return Expression{ .location = self.location, .type = .{ .number_literal = num - rhs_num } };
+                                            },
+                                            else => {
+                                                diag.emit(
+                                                    .@"error",
+                                                    op.lhs.location,
+                                                    "Expected rhs type to be a number for a subtract operation",
+                                                    .{},
+                                                );
+                                                return error.InvalidCode;
+                                            },
+                                        }
+                                    },
+                                    else => {
+                                        diag.emit(.@"error", lhs.location, "Expected type to be a number for a subtract operation", .{});
+                                        return error.InvalidCode;
+                                    },
+                                }
+                            },
+                            .multiply => {
+                                switch (lhs.type) {
+                                    .number_literal => |num| {
+                                        switch (rhs.type) {
+                                            .number_literal => |rhs_num| {
+                                                return Expression{ .location = self.location, .type = .{ .number_literal = num * rhs_num } };
+                                            },
+                                            else => {
+                                                diag.emit(
+                                                    .@"error",
+                                                    op.lhs.location,
+                                                    "Expected rhs type to be a number based for a multiply operation",
+                                                    .{},
+                                                );
+                                                return error.InvalidCode;
+                                            },
+                                        }
+                                    },
+                                    else => {
+                                        diag.emit(.@"error", lhs.location, "Expected type to be a number for a multiply operation", .{});
+                                        return error.InvalidCode;
+                                    },
+                                }
+                            },
+                            .divide => {
+                                switch (lhs.type) {
+                                    .number_literal => |num| {
+                                        switch (rhs.type) {
+                                            .number_literal => |rhs_num| {
+                                                return Expression{ .location = self.location, .type = .{ .number_literal = num / rhs_num } };
+                                            },
+                                            else => {
+                                                diag.emit(
+                                                    .@"error",
+                                                    op.lhs.location,
+                                                    "Expected rhs type to be a number based for a division operation",
+                                                    .{},
+                                                );
+                                                return error.InvalidCode;
+                                            },
+                                        }
+                                    },
+                                    else => {
+                                        diag.emit(.@"error", lhs.location, "Expected type to be a number for a division operation", .{});
+                                        return error.InvalidCode;
+                                    },
+                                }
+                            },
+                            .modulus => {
+                                switch (lhs.type) {
+                                    .number_literal => |num| {
+                                        switch (rhs.type) {
+                                            .number_literal => |rhs_num| {
+                                                return Expression{ .location = self.location, .type = .{ .number_literal = num % rhs_num } };
+                                            },
+                                            else => {
+                                                diag.emit(
+                                                    .@"error",
+                                                    op.lhs.location,
+                                                    "Expected rhs type to be a number based for a modulus operation",
+                                                    .{},
+                                                );
+                                                return error.InvalidCode;
+                                            },
+                                        }
+                                    },
+                                    else => {
+                                        diag.emit(.@"error", lhs.location, "Expected type to be a number for a modulus operation", .{});
+                                        return error.InvalidCode;
+                                    },
+                                }
+                            },
+                            .bool_or => {
+                                switch (lhs.type) {}
+                            },
+                        }
+                    }
+                }
+                return null;
+            },
+        }
+    }
 };
 
 pub const Statement = struct {
@@ -155,6 +374,17 @@ pub const Program = struct {
     pub fn deinit(self: *Self) void {
         self.arena.deinit();
         self.* = undefined;
+    }
+
+    pub fn optimize(self: *Self, alloc: std.mem.Allocator) void {
+        var consts: std.StringHashMap(Expression.ExprValue) = .init(alloc);
+        for (self.root_script) |statement| {
+            switch (statement.type) {
+                // .assignment => |assignment| {
+                //     assignment.value.
+                // }
+            }
+        }
     }
 };
 
